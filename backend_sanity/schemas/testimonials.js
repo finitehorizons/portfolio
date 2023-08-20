204935f7fdecb37700ae0586635663db3f182e02
Christopher Goodwin <goodwin.chris2021@gmail.com>
2023-08-19 23:50:30 -0500

    setup schemas for backend

--- backend_sanity/schemas/testimonials.js
+++ backend_sanity/schemas/testimonials.js
@@ -1,32 +1,30 @@
 export default {
-  name: 'testimonials',
-  title: 'Testimonials',
-  type: 'document',
-  fields: [
-    {
-      name: 'name',
-      title: 'Name',
-      type: 'string'
-    },
-    {
-      name: 'company',
-      title: 'Company',
-      type: 'string'
-    },
-    {
-      name: 'imageurl',
-      title: 'ImgURL',
-      type: 'image',
-
-      options: {
-        // Enables ability for USER to choose CROP of profile image
-        hotspot: true
-      }
-    },
-    {
-      name: 'feedback',
-      title: 'Feedback',
-      type: 'string'
-    }
-  ]
+    name:'testimonials',
+    title:'Testimonials',
+    type: 'document',
+    fields:[
+        { 
+            name:'name',
+            title:'Name',
+            type: 'string'
+        },
+        {
+            name:'company',
+            title:'Company',
+            type:'string'
+        },
+        {
+            name:'imgurl',
+            title:'ImgUrl',
+            type: 'image',
+            options: {
+              hotspot: true,
+            },
+        },  
+        {
+            name:'feedback',
+            title:'Feedback',
+            type:'string'
+        }
+    ]
 }
